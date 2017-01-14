d8b28f2f5873eb09e496fd6b8713ed3635343f4a
NearImba <747964230@qq.com>
2017-01-14 20:07:36 +0800

    多人联机 server updated

--- game.js
+++ game.js
@@ -11,14 +11,20 @@ let GL = null
 
 const vertexShader = `
     attribute vec4 a_Position;
+    attribute vec4 a_color;
+    varying vec4 vector_color;
     void main() {
         gl_Position = a_Position;
         gl_PointSize = 2.0;
+        vector_color = a_color;
     }
 `
 const fragmentShader = `
+    precision mediump float;
+    varying vec4 vector_color;
     void main() {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        gl_FragColor = vector_color;
+        //gl_FragColor = vec4(0,1,0,1);
     }
 `
 
@@ -31,30 +37,42 @@ function translateYWebToGL (y) {
 }
 
 class Snake {
-    constructor (startPoint) {
-        this.speedPerSecond = 12
+    constructor (params) {
         this.body = []
-        this.lifePoint = initSnakeLifePoint
-        this.length = 50
+
+        this.growing = 0
 
         this.N = 2 //render a circle per N
+
+        this.color = params && params.color || [parseFloat(Math.random().toFixed(2)), parseFloat(Math.random().toFixed(2)), parseFloat(Math.random().toFixed(2)), 1]
 
         this.directVertex = {
             x: 1,
             y: 0
         } // move forward vertex
 
-        let point = {
-            x: startPoint.x,
-            y: startPoint.y
+        if(params && params.hasOwnProperty('startPoint')) {
+            let point = {
+                x: params.startPoint.x,
+                y: params.startPoint.y
+            }
+
+            for(let i = 0; i < 50; i ++) {
+                this.body.unshift({
+                    x: point.x + i * R,
+                    y: point.y
+                })
+            }
+        } else if (params && params.body && params.directVertex && params.color){
+            this.directVertex = params.directVertex
+            this.body = params.body
+            this.color = params.color
+        } else {
+            console.info(params)
+            console.error('init snake params error')
+            delete this
         }
 
-        for(let i = 0; i < this.length; i ++) {
-            this.body.unshift({
-                x: point.x + i * R,
-                y: point.y
-            })
-        }
     }
 
     updateDirectVertex (targetPoint) {
@@ -91,12 +109,22 @@ class Snake {
         X1 = (X2 - X) * R / L + X
         Y1 = (Y2 - Y) * R / L + Y
 
-        this.body.pop()
+        if(this.growing === 0 || this.growing < 0) {
+            this.body.pop()
+            this.growing = 0
+        } else {
+            this.growing --
+        }
+
         this.body.unshift({
             x: X1,
             y: Y1
         })
 
+    }
+
+    grow (n) {
+        this.growing += (n || 1)
     }
 
     normalPoint () {
@@ -117,22 +145,62 @@ class Game {
         this.connectToServer()
     }
 
+    getHost () {
+        let host = /^http\:\/\/([0-9|A-z|\.|\-]*):9527/.exec(window.location.href)
+        //console.log(host)
+        return host[1]
+    }
+
     connectToServer () {
-        this.socket = io('http://localhost:9527')
-        this.socket.on('ALIVE', (data) => {
-            this.insertSnake(data);
+        this.socket = io(`http://${this.getHost()}:9527`)
+
+        this.socket.on('reject', () => {
+            alert('Too many people on server')
         })
 
-        this.socket.on('WELCOME', (data) => {
+        this.socket.on('new', (data) => {
+            this.insertSnake(data)
+        })
+
+        this.socket.on('approval', data => {
+            console.log(data)
+
+            this.insertSnake(data, true)
+
+            for(let userId in data.snakes) {
+                this.insertSnake(data.snakes[userId])
+            }
+
             this.initCanvas()
             this.initHandler()
+
+            console.log(this.snakes[this.playerID].color)
+
             this.runGame()
+        })
+
+        this.socket.on('others-input', data => {
+            this.snakes[data.id].updateDirectVertex(data.targetPoint)
+        })
+
+        this.socket.on('others-quit', data => {
+            delete this.snakes[data.id]
+        })
+    }
+
+    sendCurrentPlayerDataToServer () {
+        let currentSanke = this.snakes[this.playerID]
+        this.socket.emit('current-snake-data', {
+            id: this.playerID,
+            body: currentSanke.body,
+            directVertex: currentSanke.directVertex,
+            color: currentSanke.color
         })
     }
 
     weclomeUser (name) {
         if (name) {
-            this.socket.emit('NEW-GUY',{
+            this.socket.emit('request-in',{
                 name: name,
                 id: this.playerID
             })
@@ -147,9 +215,13 @@ class Game {
                 this.lastDataUpdatedTime = t
             }
             this.render()
-            requestAnimationFrame(function () {
+            // requestAnimationFrame(function () {
+            //     this.runGame()
+            // }.bind(this))
+
+            setTimeout(() => {
                 this.runGame()
-            }.bind(this))
+            }, 500)
         } else {
             alert('Game Over')
         }
@@ -173,15 +245,14 @@ class Game {
     initHandler () {
         let _t = this
         function handler (e) {
-            //console.log(e)
-            let X = e.clientX, Y = e.clientY
+            let X = e.layerX, Y = e.layerY
             this.snakes[this.playerID].updateDirectVertex({
                 x: X,
                 y: Y
             })
-            this.socket.emit('INPUT', {
+            this.socket.emit('current-player-input', {
                 id: this.playerID,
-                point: {
+                targetPoint: {
                     x: X,
                     y: Y
                 }
@@ -190,14 +261,32 @@ class Game {
         this.canvas.addEventListener('click', handler.bind(this), false)
     }
 
-    insertSnake (a) {
-        this.snakes[this.playerID] = (new Snake({
-            x: a.x,
-            y: a.y
-        }))
+    insertSnake (p, type) {
+        // switch (type) {
+        //     case 0 :
+        //         this.snakes[this.playerID] = new Snake(p)
+        //         break
+        //     case 1 :
+        //         this.snakes[p.playerID] = (new Snake(p))
+        //         break
+        //     case 2 :
+        //         break
+        //     default :
+        //         break
+        //
+        // }
+        // if(type) {
+        //     this.snakes[this.playerID] = (new Snake(p))
+        // } else {
+        //     this.snakes[p.id] = new Snake(p)
+        // }
+
+        this.snakes[p.id] = new Snake(p)
+
     }
 
     updateAllSnakesData () {
+        this.sendCurrentPlayerDataToServer()
         for(let i in this.snakes) {
             this.snakes[i].move()
         }
@@ -205,7 +294,8 @@ class Game {
 
     render () {
         let ap = GL.getAttribLocation(GL.program, "a_Position")
-        GL.clearColor(0, 0, 0, 1);
+        let ac = GL.getAttribLocation(GL.program, "a_color")
+        GL.clearColor(1, 1, 1, 1);
         GL.clear(GL.COLOR_BUFFER_BIT);
 
         let pts = []
@@ -215,6 +305,9 @@ class Game {
             for(let j = 0; j < B.length; j++) {
                 pts.push(translateXWebToGL(B[j].x))
                 pts.push(translateYWebToGL(B[j].y))
+                pts.push(this.snakes[key].color[0])
+                pts.push(this.snakes[key].color[1])
+                pts.push(this.snakes[key].color[2])
             }
         }
 
@@ -222,16 +315,21 @@ class Game {
 
         GL.bindBuffer(GL.ARRAY_BUFFER, vertexBuffer)
 
-        GL.vertexAttribPointer(ap, 2, GL.FLOAT, false, 0, 0)
-
         GL.enableVertexAttribArray(ap)
+        GL.enableVertexAttribArray(ac)
 
         let vArray = new Float32Array(pts)
+
+        let _SIZE = vArray.BYTES_PER_ELEMENT
+
+        GL.vertexAttribPointer(ap, 2, GL.FLOAT, false, _SIZE*5, 0)
+
+        GL.vertexAttribPointer(ac, 3, GL.FLOAT, false, _SIZE*5, _SIZE*2)
 
         GL.bufferData(GL.ARRAY_BUFFER, vArray, GL.STATIC_DRAW)
 
         GL.clear(GL.COLOR_BUFFER_BIT)
 
-        GL.drawArrays(GL.POINTS, 0 , pts.length/2);
+        GL.drawArrays(GL.POINTS, 0 , pts.length/5);
     }
 }
--- server.js
+++ server.js
@@ -3,35 +3,75 @@
 const http = require('http')
 const fs = require('fs')
 
-const app = http.createServer((request, response) =>{
-    fs.readFile(__dirname + '/index.html',
+const app = http.createServer((request, response) => {
+    fs.readFile(__dirname + request.url,
         function (err, data) {
             if (err) {
-                res.writeHead(500);
-                return res.end('Error loading index.html');
+                response.writeHead(500);
+                return response.end('Error loading index.html');
             }
 
-            res.writeHead(200);
-            res.end(data);
+            response.writeHead(200);
+            response.end(data);
     })
 }).listen(9527)
 
 const io = require('socket.io')(app)
 
+const MAXUSERNUM = 6
 let USERS = []
-let SNAKES = []
+let SNAKES = {}
 
 io.on('connection', function (socket) {
-    socket.emit('ALIVE', {
-        x: 200,
-        y: 20 + USERS.length * 30
+    if(USERS.length >= MAXUSERNUM) {
+        socket.emit('reject')
+        return
+    }
+    let ID = ''
+    //new player joined
+    socket.on('request-in', function (data) {
+        USERS.push(data)
+        ID = data.id
+
+        let color = [parseFloat(Math.random().toFixed(2)), parseFloat(Math.random().toFixed(2)), parseFloat(Math.random().toFixed(2)), 1]
+        socket.emit('approval', {
+            id: data.id,
+            users: USERS,
+            startPoint: {
+                x: 200,
+                y: 20 + USERS.length * 30
+            },
+            snakes: SNAKES,
+            color: color
+        })
+
+        socket.broadcast.emit('new', {
+            id: data.id,
+            startPoint: {
+                x: 200,
+                y: 20 + USERS.length * 30
+            },
+            color: color
+        });
     })
 
-    socket.on('NEW-GUY', function (data) {
-        USERS.push(data)
-        socket.emit('WELCOME', {
-            users: USERS,
-            snakes: SNAKES
-        })
+    //player sent data to server
+    socket.on('current-snake-data', data => {
+        SNAKES[data.id] = data
+    })
+
+    socket.on('current-player-input', data => {
+        socket.broadcast.emit('others-input', data);
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('others-quit', {id: ID});
+        for(let i = 0; i < USERS.length; i++) {
+            if(USERS[i].id === ID) {
+                USERS.splice(i, 1)
+                break
+            }
+        }
+        delete SNAKES[ID]
     })
 })
